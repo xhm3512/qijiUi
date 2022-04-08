7ffba5ae89f636883fad9d0852564de085a418d3
2444544964@qq.com <run>
2022-04-08 17:17:14 +0800

    fix:  修复部署问题

--- es/components/Layout/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-var Foo = function Foo() {
-  return /*#__PURE__*/ React.createElement('div', null, 'foo');
-};
-
-export default Foo;
--- es/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as Remark } from './components/Remark';
-export { default as Foo } from './components/Foo';
-export { default as CatchErr } from './components/CatchErr';
-export { default as Editor } from './components/Editor';
-export { default as Loadable } from './components/Loadable';
-export { default as Icons } from './components/Icons/index';
